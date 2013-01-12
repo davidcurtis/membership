86d045f5f80cd5d38c4a1fe2f598cd678688cb35
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-01-12 17:55:37 -0500

    member find component with inheritance

--- web/component/member/member_update_by_admin.dart
+++ web/component/member/member_update_by_admin.dart
@@ -1,6 +1,6 @@
 import 'package:membership/membership.dart';
 import 'package:web_ui/web_ui.dart';
 
-class MemberListUpdate extends WebComponent {
+class MemberUpdateByAdmin extends WebComponent {
   Members members;
 }
